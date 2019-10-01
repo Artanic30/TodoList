af08e74996ef3e8cda8d76e34f46195940b4b950
Artanic <599253487@qq.com>
2019-10-02 00:12:37 +0800

    partial_update test

---
+++ Backend/TodoList/testing/testcase.py
@@ -0,0 +1,31 @@
+from django.test import TestCase as DjangoTestCase
+from django.contrib.auth.models import User
+from todo.models import Events
+from datetime import datetime
+import django.utils.timezone as timezone
+from django.forms.models import model_to_dict
+
+
+class TestCase(DjangoTestCase):
+
+    def createUser(self, username):
+        user = User.objects.create(username=username, password=username + 'password')
+        user.save()
+        return user
+
+    def createEvents(self):
+        result = {
+            'ongoing': [],
+            'history': []
+        }
+        for i in range(4):
+            event = Events.objects.create(
+                title='test_event_' + str(i),
+                detail='test_detail_' + str(i),
+                priority=i,
+                expire_time=datetime.now().replace(year=datetime.now().year + 1, tzinfo=timezone.utc),
+                is_done=True if i % 2 == 0 else False
+            )
+            event.save()
+            result['history' if i % 2 == 0 else 'ongoing'].append(event)
+        return result
--- Backend/TodoList/todo/tests.py
+++ Backend/TodoList/todo/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from testing.testcase import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from .serializers import EventsSerializers
+
 
 # Create your tests here.
+
+
+class EventsTests(TestCase):
+    def setUp(self):
+        self.user = self.createUser('test_name')
+        self.events = self.createEvents()
+
+    def test_get_events(self):
+        url = '/api/'
+        client = APIClient(enforce_csrf_checks=True)
+        response = client.get(url, decode=True)
+        self.assertEqual(response.status_code, 200)
+        for pk in range(len(response.data)):
+            self.assertEqual(response.data[pk], EventsSerializers(self.events['ongoing'][pk]).data)
